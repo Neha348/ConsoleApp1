962d7db27dffb9919b75cbf0e8de16c19739f6f3
neha arora <nehaarora423@gmail.com>
2021-11-24 03:15:42 +0530

    Add project files.

---
+++ ConsoleApp1/Model.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Coding.Challenge.Firstname.Lastname
+{
+
+    public class Car
+    {
+        public string Brand
+        {
+            get;
+            set;
+        } = "Mercedes Benz";
+        public bool IsEcoFriendly { get; set; } = true;
+        public string Fuel { get; set; } = "Hybrid";
+
+        public List<string> Models { get; set; } = new List<string>()
+        {
+            "Hello",
+  "Hello2",
+  "Hello3"
+        };
+       
+       
+        //public static Car Lookup(string id)
+        //{
+        // return id;
+        //}
+    }
+}
+    
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace Coding.Challenge.Firstname.Lastname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlDocument document2 = new HtmlDocument();
+            document2.Load(@"C:\Users\hp\Template.txt");
+            var script = document2.DocumentNode.Descendants()
+                                         .Where(n => n.Name == "script")
+                                         .First().InnerText;
+            Console.WriteLine("discovered script tag:\n" + script);
+            //not able to understand what need to be taken for the information provided in script tag???
+            Car c = new Car(); 
+            var title = document2.DocumentNode.SelectSingleNode("//title");
+            if (title.InnerHtml.Contains("$"))
+
+                Console.WriteLine("discovered title tag:\n" + c.Brand);
+           
+            var innertitle = document2.DocumentNode.SelectSingleNode("//body/h1");
+
+            if (innertitle.InnerHtml.Contains("$"))
+            {
+                innertitle.Attributes["title"].Value = c.Brand;
+                Console.WriteLine(" title :\n" + innertitle.Attributes["title"].Value);
+                Console.WriteLine("discovered title tag:\n" + c.Brand);
+            }
+
+            if (c.IsEcoFriendly == true)
+            {
+                var node = document2.DocumentNode.SelectSingleNode("//body/h2");
+                if (node.InnerHtml.Contains("$"))
+                    Console.WriteLine("discovered fuel:\n" + c.Fuel);
+                else
+                {}
+                //No need of rendering
+
+            }
+                var model = document2.DocumentNode.SelectSingleNode("//div");
+                if(model.Attributes.Contains("data-repeat-model"))
+                {
+                    //iteration comes in here. 
+                    foreach (string s in c.Models)
+                        Console.WriteLine(s);
+                }
+             
+                
+            }
+
+            }
+
+    }
+
